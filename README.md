5daa75a8a0c68a49e247c17a1a608d6580444041
Martyn <m@rtyn.berlin>
2021-05-14 18:40:01 +0200

    Enable import and prepare for public

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # page4streamersonglist
-Page for streamersonglist
+
+A cloudflare-pages-enabled repo for setting up an image overlay based on tags on streamersonglist.
+
+This doesn't and won't ever replace the overlay functionality within streamersonglist, it just allows you to have another overlay that's based on tags.
+
+For instance you might want an image to appear on your stream for your originals.
+
+You probably don't need this repo, as it deploys to https://page4streamersonglist.pages.dev/ and you can put your images somewhere else, even.
--- static/js/overlaygen.js
+++ static/js/overlaygen.js
@@ -1,5 +1,36 @@
 function importURL() {
-    alert("unimplemented");
+    
+    function paramsToObject(entries) {
+        const result = {}
+        for(const [key, value] of entries) { // each 'entry' is a [key, value] tupple
+            result[key] = value;
+        }
+        return result;
+    }
+
+    let existing = document.getElementById('existing').value;
+    let obj = null;
+    if (existing.indexOf('#') > 0) {
+        const paramsString = existing.substring(existing.indexOf("#")+1);
+        const searchParams = new URLSearchParams(paramsString);
+        const entries = searchParams.entries();
+        obj = paramsToObject(entries);
+    }
+    if (typeof obj.streamer != "undefined") {
+        document.getElementById('streamername').value = obj.streamer;  
+    }
+    if (typeof obj.basedirForImages != "undefined") {
+        document.getElementById('basedirForImages').value = obj.basedirForImages; 
+    }
+    if (typeof obj.onlyTags != "undefined") {
+        document.getElementById('onlytags').value = obj.onlyTags; 
+    }
+    if (typeof obj.interval != "undefined") {
+        document.getElementById('interval').value = obj.interval; 
+    }
+    if (typeof obj.addSongImages != "undefined") {
+        document.getElementById('addSongImages').value = atob(obj.addSongImages); 
+    }
 }
 
 function generateURL() {
@@ -18,5 +49,4 @@ function generateURL() {
         finalurl += "&interval="+document.getElementById('interval').value
     }
     document.getElementById("overlayUrl").innerHTML = '<a href="'+finalurl+'">'+finalurl+'</a>';
-    alert(finalurl)
 }
